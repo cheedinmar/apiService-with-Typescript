4c1283f0e862b759874ba4de064ee4a56af99ce1
cheedinmar <cheedinmarblessing@gmail.com>
2022-01-26 23:03:09 +0100

    remove extra button and fix UI

--- src/views/login.vue
+++ src/views/login.vue
@@ -18,21 +18,8 @@
 						<label class="block text-md mb-2" for="password">Password</label>
 						<input class="px-4 w-full border-2 py-2 rounded-md text-sm outline-none" type="password" v-model="password" name="password" placeholder="password">
         </div>
-
-							<div class="flex justify-between">
-								<div>
-									<input class="cursor-pointer"  type="radio" name="rememberme">
-									<span class="text-sm">Remember Me</span>
-								</div>
-								<span class="text-sm text-blue-700 hover:underline cursor-pointer">Forgot password?</span>
-							</div>
 							<div class="">
 								<button class="mt-4 mb-3 w-full bg-green-500 hover:bg-green-400 text-white py-2 rounded-md transition duration-100" @click.prevent="login">Login now</button>
-								<div class="flex  space-x-2 justify-center items-end bg-gray-700 hover:bg-gray-600 text-white py-2 rounded-md transition duration-100">
-
-            <img class=" h-5 cursor-pointer" src="https://i.imgur.com/arC60SB.png" alt="">
-									<button >Or sign-in with google</button>
-								</div>
 							</div>
 				</form>
 				<p class="mt-8"> Dont have an account? <span class="cursor-pointer text-sm text-blue-600"> Join free today</span></p>
